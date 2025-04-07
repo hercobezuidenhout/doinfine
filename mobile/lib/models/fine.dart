17d3fe2963b03604ff2cec62b7339d4584767667
Herco Bezuidenhout <hercobez@icloud.com>
2025-04-07 11:38:03 +0200

    Initial commit

--- mobile/lib/models/fine.dart
+++
@@ -1,8 +0,0 @@
-import 'package:doinfine/models/post_meta_data.dart';
-import 'package:doinfine/models/profile.dart';
-
-class Fine extends PostMetaData {
-  final Profile issuedTo;
-
-  Fine({required this.issuedTo});
-}

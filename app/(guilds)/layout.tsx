8aeddcdfe7429aa0ea4e250fec3cdd3de94df4a8
Herco Bezuidenhout <hercobez@icloud.com>
2025-08-10 14:56:08 +0200

    feat: Add icon to Guilds

--- app/(guilds)/layout.tsx
+++ app/(guilds)/layout.tsx
@@ -1,6 +1,7 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import { Heading, HStack, Icon, VStack } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 import { BackButton } from "./components/BackButton";
+import { LuShieldHalf } from "react-icons/lu";
 
 export default function GuildsLayout({ children }: PropsWithChildren) {
 
@@ -8,7 +9,11 @@ export default function GuildsLayout({ children }: PropsWithChildren) {
         <VStack alignItems="stretch" gap={4} padding={4}>
             <VStack alignItems="start" width="full">
                 <BackButton />
-                <Heading size="2xl">Guilds</Heading>
+                <HStack>
+                    <Icon size="lg"><LuShieldHalf /></Icon>
+                    <Heading size="2xl">
+                        Guilds</Heading>
+                </HStack>
             </VStack>
             {children}
         </VStack>

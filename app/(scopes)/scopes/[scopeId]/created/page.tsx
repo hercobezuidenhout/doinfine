3e85cfd76f56a6648d1ff487946b5ad442cda356
Herco Bezuidenhout <hercobez@icloud.com>
2025-09-09 20:01:18 +0200

    feat: Refactor success page layout for improved structure

--- app/(scopes)/scopes/[scopeId]/created/page.tsx
+++ app/(scopes)/scopes/[scopeId]/created/page.tsx
@@ -3,29 +3,33 @@ import { Button, Card, Clipboard, Heading, IconButton, Input, InputGroup, Stack,
 export default function Page() {
 
     return (
-        <VStack gap={8} pt={16}>
-            <VStack gap={2}>
-                <Heading size="2xl">🙌</Heading>
-                <Heading>Success!</Heading>
-            </VStack>
-            <Text textAlign="center">
-                Your group has been created.
-                You can share the below link with your friends to invite them to the group.
-            </Text>
-            <Clipboard.Root width="full" value="https://chakra-ui.com">
-                <InputGroup endElement={
-                    <Clipboard.Trigger asChild>
-                        <IconButton variant="surface" size="xs" me="-2">
-                            <Clipboard.Indicator />
-                        </IconButton>
-                    </Clipboard.Trigger>
-                }>
-                    <Clipboard.Input asChild>
-                        <Input />
-                    </Clipboard.Input>
-                </InputGroup>
-            </Clipboard.Root>
-            <Button>Continue to group</Button>
-        </VStack>
+        <>
+            <Card.Root>
+                <VStack gap={8} pt={16}>
+                    <VStack gap={2}>
+                        <Heading size="2xl">🙌</Heading>
+                        <Heading>Success!</Heading>
+                    </VStack>
+                    <Text textAlign="center">
+                        Your group has been created.
+                        You can share the below link with your friends to invite them to the group.
+                    </Text>
+                    <Clipboard.Root width="full" value="https://chakra-ui.com">
+                        <InputGroup endElement={
+                            <Clipboard.Trigger asChild>
+                                <IconButton variant="surface" size="xs" me="-2">
+                                    <Clipboard.Indicator />
+                                </IconButton>
+                            </Clipboard.Trigger>
+                        }>
+                            <Clipboard.Input asChild>
+                                <Input />
+                            </Clipboard.Input>
+                        </InputGroup>
+                    </Clipboard.Root>
+                </VStack>
+            </Card.Root>
+            <Button mt={6}>Continue to group</Button>
+        </>
     );
 }

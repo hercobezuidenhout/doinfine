afce7e9ece1eb3a53bd20df93c39ebe01c0f0a42
Herco Bezuidenhout <hercobez@icloud.com>
2025-09-09 20:07:11 +0200

    feat: Enhance success page layout for better readability and structure

--- app/(scopes)/scopes/[scopeId]/created/page.tsx
+++ app/(scopes)/scopes/[scopeId]/created/page.tsx
@@ -4,32 +4,37 @@ export default function Page() {
 
     return (
         <>
-            <Card.Root>
-                <VStack gap={8} pt={16}>
-                    <VStack gap={2}>
-                        <Heading size="2xl">🙌</Heading>
-                        <Heading>Success!</Heading>
+            <Card.Root variant="subtle">
+                <Card.Body>
+                    <VStack gap={8}>
+                        <VStack gap={2}>
+                            <Heading size="2xl">🙌</Heading>
+                            <Heading>Success!</Heading>
+                        </VStack>
+                        <Text textAlign="center">
+                            Your group has been created.
+                            You can share the below link with your friends to invite them to the group.
+                        </Text>
+                        <Clipboard.Root width="full" value="https://chakra-ui.com">
+                            <InputGroup endElement={
+                                <Clipboard.Trigger asChild>
+                                    <IconButton variant="surface" size="xs" me="-2">
+                                        <Clipboard.Indicator />
+                                    </IconButton>
+                                </Clipboard.Trigger>
+                            }>
+                                <Clipboard.Input asChild>
+                                    <Input variant="outline" />
+                                </Clipboard.Input>
+                            </InputGroup>
+                        </Clipboard.Root>
                     </VStack>
-                    <Text textAlign="center">
-                        Your group has been created.
-                        You can share the below link with your friends to invite them to the group.
-                    </Text>
-                    <Clipboard.Root width="full" value="https://chakra-ui.com">
-                        <InputGroup endElement={
-                            <Clipboard.Trigger asChild>
-                                <IconButton variant="surface" size="xs" me="-2">
-                                    <Clipboard.Indicator />
-                                </IconButton>
-                            </Clipboard.Trigger>
-                        }>
-                            <Clipboard.Input asChild>
-                                <Input />
-                            </Clipboard.Input>
-                        </InputGroup>
-                    </Clipboard.Root>
-                </VStack>
+                </Card.Body>
             </Card.Root>
-            <Button mt={6}>Continue to group</Button>
+            <VStack alignItems="stretch" gap={2}>
+                <Button>Continue</Button>
+                <Button variant="outline">Share</Button>
+            </VStack>
         </>
     );
 }

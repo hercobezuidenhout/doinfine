3dffb69f20259bbc4c3659effc6fe906f41254d4
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2025-09-14 18:06:44 +0200

    refactor: Update Heading size in Home component for responsive design

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,11 @@
-import { Heading, VStack } from "@chakra-ui/react";
-import { Feed } from "../components/Feed";
+import { Heading, VStack } from "@chakra-ui/react"
+import { Feed } from "../components/Feed"
 
 export default function Home() {
     return (
         <VStack alignItems="stretch">
-            <Heading size="5xl">Feed</Heading>
+            <Heading size={{ base: "3xl", md: "5xl" }}>Feed</Heading>
             <Feed />
         </VStack>
-    );
+    )
 }

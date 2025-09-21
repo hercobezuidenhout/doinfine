4c7c7c40a599d53408774675fa077e44a0d8862f
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2025-09-21 17:49:35 +0200

    refactor: Update manfiest file

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,16 +4,21 @@ export default function manifest(): MetadataRoute.Manifest {
     return {
         name: 'Doinfine',
         short_name: 'Doinfine',
-        description: 'Fine your friends.',
+        description: 'An app that lets you fine your friends.',
         start_url: '/',
         display: 'standalone',
-        background_color: '#fff',
-        theme_color: '#fff',
+        background_color: '#ffffff',
+        theme_color: '#000000',
         icons: [
             {
-                src: '/favicon.ico',
-                sizes: 'any',
-                type: 'image/x-icon',
+                src: '/icons/icon-192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/icons/icon-512.png',
+                sizes: '512x512',
+                type: 'image/png',
             },
         ],
     };

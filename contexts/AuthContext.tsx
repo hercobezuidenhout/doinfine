9b20dc52005cb466e6fb1772fc5254146f1d59c7
Herco Bezuidenhout <hercobez@icloud.com>
2025-11-27 14:26:31 +0200

    Update AuthContext to hook into auth state

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { useCurrentUserQuery } from "@/queries/useCurrentUserQuery";
+import { createClient } from "@/utils/supabase/client";
 import { Center, Spinner } from "@chakra-ui/react";
 import { User } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
-import { createContext, useContext, ReactNode, useEffect } from "react";
+import { createContext, useContext, ReactNode, useEffect, useState } from "react";
 
 interface AuthContextValue {
     user: User | undefined | null;
@@ -13,28 +14,26 @@ interface AuthContextValue {
 const AuthContext = createContext<AuthContextValue | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode; }) => {
-    const { data: user, isLoading, isFetched } = useCurrentUserQuery();
+    const { auth } = createClient();
+    const [user, setUser] = useState<User>();
     const router = useRouter();
 
     useEffect(() => {
-        if (isFetched || isLoading) return;
-        if (!user?.data.user) {
-            router.push('/login');
-        }
-    }, [isLoading, isFetched, user, router]);
+        auth.onAuthStateChange((event, session) => {
+            console.info(event);
+            // You can handle auth state changes here if needed
+            if (!session) {
+                router.push('/login');
+                return;
+            }
 
-    if (!isLoading && !user?.data.user) {
-        return null;
-    }
+            setUser(session.user);
+        });
+    }, [auth, router]);
 
-    return (
-        <AuthContext.Provider value={{ user: user?.data.user }}>
-            {!isLoading && children}
-            {isLoading && (
-                <Center height="95vh">
-                    <Spinner />
-                </Center>
-            )}
+    return user && (
+        <AuthContext.Provider value={{ user: user }}>
+            {children}
         </AuthContext.Provider>
     );
 };
